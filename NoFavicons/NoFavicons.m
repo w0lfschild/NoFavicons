b276cd04bccbf0741be24d788829af943ef43a10
w0lfschild <aguywithlonghair@yahoo.com>
2017-08-03 00:18:43 -0700

    1.7.0
    - Major re-write to support Chrome 60+
    - Add new hash for new folder icon
    - Removed old code (haven't tested support on old chrome/osx yet)

--- NoFavicons/NoFavicons.m
+++ NoFavicons/NoFavicons.m
@@ -1,4 +1,5 @@
 #import "BookmarkButtonCell.h"
+#import "BookmarkButton.h"
 #import "ZKSwizzle.h"
 #import <CommonCrypto/CommonDigest.h>
 
@@ -6,6 +7,8 @@
 
 @interface NoFavicons : NSObject
 @end
+
+/*------------------------------------*/
 
 NoFavicons *plugin;
 
@@ -24,56 +27,124 @@ NoFavicons *plugin;
     NSLog(@"NoFavicons loading...");
     plugin = [NoFavicons sharedInstance];
     ZKSwizzle(wb_BookmarkButtonCell, BookmarkButtonCell);
+    ZKSwizzle(wb_BookmarkButton, BookmarkButton);
     NSLog(@"%@ loaded into %@ on macOS 10.%ld", [self class], [[NSBundle mainBundle] bundleIdentifier], [[NSProcessInfo processInfo] operatingSystemVersion].minorVersion);
 }
 
 @end
+
+/*------------------------------------*/
+
+
+@interface wb_BookmarkButton : NSButton
+@end
+
+@implementation wb_BookmarkButton
+
+// Adjust the frame size to be the size the title plus padding to account for removal of image
+- (void)setFrame:(NSRect)frame {
+    CGRect newFrame = frame;
+    if ([self isInBar]) {
+        NSSize titleSize = [[self attributedTitle] size];
+        newFrame.size.width = ceil(titleSize.width + 10);
+    }
+    ZKOrig(void, newFrame);
+}
+
+//- (void)draggingSession:(id)arg1 endedAtPoint:(struct CGPoint)arg2 operation:(unsigned long long)arg3 {
+//    ZKOrig(void, arg1, arg2, arg3);
+//}
+
+// Manually work some magic to allign all the buttons ✨
+- (void)setFrameOrigin:(NSPoint)newOrigin {
+//    NSLog(@"wb_ %hhd", ZKHookIvar(self, BOOL, "dragPending_"));
+    CGPoint origin = newOrigin;
+    if ([self isInBar]) {
+        
+        // Make an array of all buttons in bookmark bar
+        NSMutableArray *buttons = [[NSMutableArray alloc] initWithArray:[[self superview] subviews]];
+        NSMutableArray *sortButtons = [[NSMutableArray alloc] init];
+        
+        // Try to weed out any odd buttons that don't belong
+        for (BookmarkButton *bm in buttons) {
+            if ([bm respondsToSelector:@selector(title)]) {
+                if ([[bm title] length]) {
+                    if (bm.frame.origin.x > 5 && bm.frame.origin.y == 4) {
+                        [bm setTag:bm.frame.origin.x];
+                        [sortButtons addObject:bm];
+                    }
+                }
+            }
+        }
+        
+        // Sort the buttons by x position from smallest to largest
+        NSSortDescriptor *sortDescriptor;
+        sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"tag" ascending:YES];
+        NSArray *final = [[NSArray alloc] initWithArray:sortButtons];
+        final = [final sortedArrayUsingDescriptors:@[sortDescriptor]];
+        
+        // Loop through the button list until we match the current button
+        if (final.count > 1) {
+            for (int i = 1; i < (final.count - 1); i++) {
+                if ([self isEqualTo:[final objectAtIndex:i]]) {
+                    NSObject *prev = [final objectAtIndex:i-1];
+                    BookmarkButton *test = (BookmarkButton*)prev;
+                    int newX = test.frame.origin.x + test.frame.size.width + 2;
+                    origin.x = newX;
+//                    NSLog(@"wb_ %@ : %@ : %@ : %@", [self title], NSStringFromRect(self.frame), test.title, NSStringFromRect(test.frame));
+                    break;
+                }
+            }
+        }
+    }
+    ZKOrig(void, origin);
+}
+
+// Check if bookmark is in the bookmarks bar and not in a subfolder
+- (BOOL)isInBar {
+    BOOL result = false;
+    if ([[self superview].className isEqualToString:@"BookmarkBarView"])
+        result = true;
+    return result;
+}
+
+@end
+
+/*------------------------------------*/
+
 
 @interface wb_BookmarkButtonCell : NSButtonCell
 @end
 
 @implementation wb_BookmarkButtonCell
 
-static NSString *minWidth = @"wb_NF_minWidth";
 static NSArray *Folderhashes = nil;
 
-- (struct CGRect)imageRectForBounds:(struct CGRect)arg1 {
-    // Remove Image
-    if ([[self title] isEqualToString:@""])
-        return ZKOrig(struct CGRect, arg1);
-    return CGRectZero;
-}
-
-- (double)textStartXOffset {
-    // Proper text placement
-    return 5.0;
-}
-
-- (struct CGSize)cellSize {
-    // Prevent this from resizing larger once the initial size is set above 0
-    // Buttons will still resize when edited but this prevents a resize when the favicon is first loaded
-    CGSize result = ZKOrig(struct CGSize);
-    int oldsize = [objc_getAssociatedObject(self, minWidth) intValue];
-    int newsize = result.width;
-    if (oldsize == 0)
-        objc_setAssociatedObject(self, minWidth, [NSNumber numberWithInt:newsize], OBJC_ASSOCIATION_RETAIN);
-    if (newsize <= oldsize)
-        objc_setAssociatedObject(self, minWidth, [NSNumber numberWithInt:newsize], OBJC_ASSOCIATION_RETAIN);
-    if (oldsize != 0)
-        result.width = oldsize;
+// Make the title start offset 5 and width to the frame size minus 10 for the padding
+- (struct CGRect)titleRectForBounds:(struct CGRect)arg1 {
+    CGRect result = ZKOrig(struct CGRect, arg1);
+    result.origin.x = 5;
+    result.size.width = arg1.size.width - 10;
     return result;
 }
 
-- (void)setBookmarkCellText:(id)arg1 image:(id)arg2 {
-    if ([self imageIsFolder:arg2])
-        if (![(BookmarkButtonCell*)self isFolderButtonCell])
-            arg1 = [NSString stringWithFormat:@"%@ ▾", arg1];
-    ZKOrig(void, arg1, [NSImage new]);
-    if (NSProcessInfo.processInfo.operatingSystemVersion.minorVersion == 9)
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{ ZKOrig(void, arg1, [NSImage new]); });
+// Hide the button image
+- (struct CGRect)imageRectForBounds:(struct CGRect)arg1 {
+    return CGRectMake(4, -1, 0, 0);
 }
 
-- (BOOL) imageIsFolder: (NSImage*)image {
+// Add " ▾" to folders in the bookmar bar
+- (void)setTitle:(NSString *)title {
+    if (![(BookmarkButtonCell*)self isFolderButtonCell]) {
+        if ([self imageIsFolder:[self image]])
+            if (![title hasSuffix:@" ▾"])
+                title = [NSString stringWithFormat:@"%@ ▾", title];
+    }
+    ZKOrig(void, title);
+}
+
+// Check if a button image is a folder image
+- (BOOL)imageIsFolder:(NSImage*)image {
     if (Folderhashes == nil) {
         Folderhashes = [[NSArray alloc] initWithObjects:
                         @"4cc0afe6dc8a0cee961f15b087f6cc13784f6bad",
@@ -81,6 +152,7 @@ static NSArray *Folderhashes = nil;
                         @"e7bb69efe7575935236c0dacee014df026acbf8f",
                         @"fb95ab18021d0833b84ae893210edf05f85ba24d",
                         @"4fd0758c661623035ecf46b55ad13e6c5dec1fa4",
+                        @"22cd19df85c313dcefe1342849ffdb1d7928388e",
                         nil];
     }
     NSData* data = [image TIFFRepresentation];
@@ -89,7 +161,7 @@ static NSArray *Folderhashes = nil;
     NSMutableString* sha1 = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
         [sha1 appendFormat:@"%02x", digest[i]];
-//    NSLog(@"wb_ %lu : %@", (unsigned long)self.hash, sha1);
+//    NSLog(@"wb_ %@ : %lu : %@", [self title], (unsigned long)self.hash, sha1);
     return ([Folderhashes indexOfObject:sha1] != NSNotFound);
 }
 
